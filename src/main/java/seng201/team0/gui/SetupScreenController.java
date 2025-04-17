65892aa63cb176b8bff48329ef214256dad4f15c
Dwyane <dra131@uclive.ac.nz>
2025-04-17 15:04:19 +1200

    Refined the select screen. Changes include: - Adding buttons to navigate towards the different options - New grid panes for selecting season length and inputting your name - Improved the aesthetics via adding a background and changing the text.

--- src/main/java/seng201/team0/gui/SetupScreenController.java
+++ src/main/java/seng201/team0/gui/SetupScreenController.java
@@ -3,13 +3,15 @@ package seng201.team0.gui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class SetupScreenController {
     @FXML
@@ -29,7 +31,36 @@ public class SetupScreenController {
     private ImageView hardPic;
 
     @FXML
+    private ImageView leftArrow;
+    @FXML
+    private ImageView rightArrow;
+
+    @FXML
     private Slider diffSlider;
+    @FXML
+    private Slider seasonSlider;
+
+    @FXML
+    private Label seasonCountLabel;
+
+    @FXML
+    private TextField inputNameArea;
+    @FXML
+    private Text whatNameLabel;
+
+    @FXML
+    private GridPane titleGridPane;
+    @FXML
+    private GridPane chooseDifficultyGridPane;
+    @FXML
+    private GridPane chooseCarGridPane;
+    @FXML
+    private GridPane finalSelectScreenGridPane;
+    @FXML
+    private GridPane chooseSeasonLengthGridPane;
+    @FXML
+    private GridPane chooseNameGridPane;
+
 
 
     static private Map<String, String> difficulties = Map.of(
@@ -37,6 +68,37 @@ public class SetupScreenController {
             "Regular", "Not too easy, not too forgiving",
             "Hard", "For the racers with something to prove"
     );
+
+    private int raceCount = 3;
+
+
+
+    static int optionIndex = 0;
+    public void showNewOptionSlide() {
+        List<GridPane> chooseOptions = Arrays.asList(chooseNameGridPane, chooseDifficultyGridPane,chooseSeasonLengthGridPane);
+        GridPane newPaneToShow = chooseOptions.get(optionIndex);
+
+        newPaneToShow.setVisible(true);
+        for(GridPane option : chooseOptions) {
+            if (option != newPaneToShow) {
+                option.setVisible(false);
+            }
+        }
+
+    }
+
+    public void moveRight(MouseEvent mouseEvent) {
+        System.out.println("MOVING RIGHT");
+        optionIndex++;
+        showNewOptionSlide();
+    }
+
+    public void moveLeft(MouseEvent mouseEvent) {
+        System.out.println("MOVING LEFT");
+        optionIndex--;
+        showNewOptionSlide();
+    }
+
 
     static private String chosenDifficulty = "Regular";
 
@@ -50,6 +112,12 @@ public class SetupScreenController {
 
     public String getChosenDifficulty() {
         return chosenDifficulty;
+    }
+
+
+    private void updateSeasonCount(int newRaceCount) {
+        raceCount = newRaceCount;
+        seasonCountLabel.setText("Number of races: " + raceCount);
     }
 
     private void updateDifficulty(int newDiffLevel) {
@@ -79,11 +147,18 @@ public class SetupScreenController {
 
     public void initialize(Stage stage) {
         diffDesc.setText(getDifficultyDesc());
+        showNewOptionSlide();
         diffSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
             updateDifficulty(newValue.intValue());
+        });
+        seasonSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            updateSeasonCount(newValue.intValue());
         });
 
 
     }
 
+    public void bruh(MouseEvent mouseEvent) {
+        System.out.println("BRUH");
+    }
 }
